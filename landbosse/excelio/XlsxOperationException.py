17cc1d62a6c7e0e8354765e50fb7308da6c8b8d6
Alicia Key <alicia.key@nrel.gov>
2019-11-26 15:29:33 -0700

    Update XlsxOperationException.py
    Fix a typo.

--- landbosse/excelio/XlsxOperationException.py
+++ landbosse/excelio/XlsxOperationException.py
@@ -3,7 +3,7 @@ class XlsxOperationException(Exception):
     This exception is raised for errors that occur when processing .xlsx
     spreadsheet files.
 
-    It has no custom implmentation. It is here to provide a class that
+    It has no custom implementation. It is here to provide a class that
     can be specifically caught in an except statement.
 
     See also: https://docs.python.org/3/library/exceptions.html
